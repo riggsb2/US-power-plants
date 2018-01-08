587469fb51740c5fc30668ed2da01d6101b17ea8
riggsb2 <riggsb2@gmail.com>
2018-01-08 15:36:08 -0500

    Portofolio files generate. Base analysis formed

--- Power_plant.py
+++ Power_plant.py
@@ -441,7 +441,7 @@ for year in years:
 if not os.path.isdir(os.path.join('Results')):
     os.makedirs(os.path.join('Results'))
 os.chdir(os.path.join('Results'))
-
+'''
 print('Retail Pricing Trends')
 RetailTrends = RetailPricingTrend(retail_prices_df,5) 
 RetailTrends.to_csv('Retail Pricing Trends.csv',delimiter = ',')
@@ -450,16 +450,16 @@ print('Efficiency Trends')
 Efftrends = MoverTrends(master_df,'Efficiency',5)
 Efftrends.to_csv('Efficiency Trend df.csv',delimiter = ',')
 TrendBoxplot(Efftrends,'Efficiency',0.7,-0.25,0.25)
-'''
+
 print('Selling Price Trends')
 Pricetrends = MoverTrends(master_df,'Price of El',4)
 Pricetrends.to_csv('Selling Price Trend df.csv',delimiter = ',')
 TrendBoxplot(Pricetrends,'Price of El',0.6,-0.25,0.25)
-'''
+
 print('State Portfolio')
 EnergyPort = StatePortfolio(master_df)
 EnergyPort.to_csv('State distribution df.csv',delimiter = ',')
 PortfolioTrends = PortfolioTrend(EnergyPort)
 PortfolioTrends.to_csv('State portfolio trends.csv',delimiter = ',')
 PortfolioPlot(EnergyPort)
-
+'''
